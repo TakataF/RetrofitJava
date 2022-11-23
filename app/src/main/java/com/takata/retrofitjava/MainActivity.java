b2da188f64d173e5b926da036a17779f701f0b8f
Felipe Takata de Alvarenga <felipetakatadealvarenga@gmail.com>
2022-11-23 15:11:10 -0300

    Feat Spinner in MainActivity

--- app/src/main/java/com/takata/retrofitjava/MainActivity.java
+++ app/src/main/java/com/takata/retrofitjava/MainActivity.java
@@ -3,12 +3,20 @@ package com.takata.retrofitjava;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    Spinner menu_cidade = (Spinner) findViewById(R.id.spinner_cidade);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.city_array, android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        menu_cidade.setAdapter(adapter);
     }
 }
