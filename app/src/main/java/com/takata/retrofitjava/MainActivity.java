be5cb3af63268686e90381d58d6ffa04cf7b1f68
gmilanimota <gustavo.milani22@gmail.com>
2022-11-24 15:43:19 -0300

    feat: start API Consumer Activity with city intent

--- app/src/main/java/com/takata/retrofitjava/MainActivity.java
+++ app/src/main/java/com/takata/retrofitjava/MainActivity.java
@@ -11,32 +11,36 @@ import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button bt_consultar;
-
-    Spinner menu_cidade = (Spinner) findViewById(R.id.spinner_cidade);
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        IniciarComponentes();
-
+        Spinner menu_cidade = (Spinner) findViewById(R.id.spinner_cidade);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.city_array, android.R.layout.simple_spinner_dropdown_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         menu_cidade.setAdapter(adapter);
 
-        bt_consultar.setOnClickListener(new View.OnClickListener() {
+        Button btn_search = findViewById(R.id.btn_consultar);
+        btn_search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, WeatherActivity.class);
+                String city_name = menu_cidade.getSelectedItem().toString();
+                String woeid = "";
+
+                if (city_name == "Umuarama") {
+                    woeid = "455918"; //Umuarama
+                } else if (city_name == "Maringá") {
+                    woeid = "455883"; //Maringá
+                } else if (city_name == "Londrina"){
+                    woeid = "455832"; //Londrina
+                }
+
+                intent.putExtra(Intent.EXTRA_TEXT, woeid);
                 startActivity(intent);
-                finish();
+
             }
         });
-    }
-
-    private void IniciarComponentes(){
-        bt_consultar = findViewById(R.id.bt_consulta);
     }
 }
